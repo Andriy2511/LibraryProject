13c9a9d01da780b8fae638c7627fcc671f22297d
Andrii Sirko <andriy251101@gmail.com>
2023-08-11 15:46:15 +0300

    The author view was implemented

--- src/main/java/com/example/library/controller/admin/AuthorController.java
+++ src/main/java/com/example/library/controller/admin/AuthorController.java
@@ -1,7 +1,26 @@
 package com.example.library.controller.admin;
 
+import com.example.library.repository.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/author")
 public class AuthorController {
+
+    private final AuthorRepository authorRepository;
+
+    @Autowired
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @GetMapping("/showAuthorList")
+    public String showBookList(Model model) {
+        model.addAttribute("authors", authorRepository.findAll());
+        return "admin/author-list";
+    }
 }
