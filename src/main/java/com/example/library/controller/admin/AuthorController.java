b4e86bca4aef1f242ecb1de05b97b42329b958d8
Andrii Sirko <andriy251101@gmail.com>
2023-08-11 23:16:33 +0300

    The option to add an author was implemented.

--- src/main/java/com/example/library/controller/admin/AuthorController.java
+++ src/main/java/com/example/library/controller/admin/AuthorController.java
@@ -1,11 +1,16 @@
 package com.example.library.controller.admin;
 
+import com.example.library.DTO.BookDTO;
+import com.example.library.model.Author;
+import com.example.library.model.Book;
+import com.example.library.model.BookCount;
 import com.example.library.repository.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/author")
@@ -22,5 +27,17 @@ public class AuthorController {
     public String showBookList(Model model) {
         model.addAttribute("authors", authorRepository.findAll());
         return "admin/author-list";
+    }
+
+    @GetMapping("/showAddAuthorForm")
+    public String showAddAuthorForm(Model model) {
+        model.addAttribute("author", new Author());
+        return "admin/add-author";
+    }
+
+    @PostMapping("/addAuthor")
+    public String addAuthor(@ModelAttribute("author") Author author) {
+        authorRepository.save(author);
+        return "redirect:/author/showAddAuthorForm";
     }
 }
